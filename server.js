7ddeeb88b0a10fbb5775ef00e43cda28bd18362e
effsolplaatjie-ux <effsolplaatjie@gmail.com>
2026-02-21 01:12:15 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -111,4 +111,4 @@ app.post('/api/policies/public-create', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
